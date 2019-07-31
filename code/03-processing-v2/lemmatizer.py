d998fba8f2f3a27c1f2a11f85a13cbe781d42143
Keton Kakkar <ketonkakkar@gmail.com>
2019-07-31 17:53:15 -0400

    csv for lemmatizer

--- code/03-processing-v2/lemmatizer.py
+++ code/03-processing-v2/lemmatizer.py
@@ -17,3 +17,24 @@ def lemmatize_all_files(directory, extension):
             obj = read_file_to_list(directory + filename)
             obj = lemmatize(obj, nlp)
             write_file(obj, directory + filename, ".lemmatized")
+
+def lemmatize_csv(input_file, output_file, nlp):
+    """
+    """
+    try:
+        with open(path) as read_fh, open(output_path, 'w') as write_fh:
+            nlp = spacy.load('en', disable=['parser','ner'])
+
+            reader = csv.reader(read_fh, delimiter=',')
+            writer = csv.writer(write_fh)
+
+            for line in reader:
+                # write line out to a different file with junky words removed
+                index = line[1]
+                doc = nlp(line[0].strip())
+                cleaned = " ".join([token.lemma_ for token in doc]))
+                row = [cleaned, index]
+                writer.writerow(row)
+
+    except (IOError, OSError):
+        print("Error opening / processing file")
