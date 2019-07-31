d5e288c6951597a83a9079c023d7d4acca9758e4
Keton Kakkar <ketonkakkar@gmail.com>
2019-07-31 11:30:26 -0400

    top sentences

--- code/06-threshold/top_sentence_indices.py
+++ code/06-threshold/top_sentence_indices.py
@@ -4,7 +4,7 @@ import pandas as pd
 import csv
 
 def get_indices_and_values(matrix_file, thresh):
-    data = np.loadtxt(matrix_file, delimeter=',')
+    data = np.loadtxt(matrix_file, delimiter=',')
 
     values = data[data>thresh]
     index1, index2 = np.nonzero(data>thresh)
