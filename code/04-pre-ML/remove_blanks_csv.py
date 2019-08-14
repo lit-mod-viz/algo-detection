b925b96ca8ba4479b31685125551c3571b73cc05
Keton Kakkar <ketonkakkar@gmail.com>
2019-08-14 14:26:42 -0400

    separate code for removing blanks from txt and csv into own respective files

--- code/04-pre-ML/remove_blanks_csv.py
+++ code/04-pre-ML/remove_blanks_csv.py
@@ -8,21 +8,23 @@ def read_file(file_object):
         data = file_object.readline()
         if not data:
             break
-        yield data.strip()
+        yield data
 
-def remove_blanks(path, output_path):
+def remove_blanks_csv(path, output_path):
     """
     """
     try:
         with open(path) as read_fh, open(output_path, 'w') as write_fh:
-            for line in read_file(read_fh):
+            reader = csv.reader(read_fh, delimiter=',')
+            writer = csv.writer(write_fh)
+            
+            for line in reader:
                 # write line out to a different file with blanks removed
-                if line:
-                    print(line)
-                    cleaned = " ".join([word for word in line.split() if word != ''])
-                    write_fh.write(cleaned)
-                    write_fh.write('\n')
-
+                index = line[1]
+                if line[0] != '':
+                    cleaned = " ".join([word for word in line[0].split() if word != ''])
+                    row = [cleaned, index]
+                    writer.writerow(row)
     except (IOError, OSError):
         print("Error opening / processing file")
 
@@ -34,7 +36,8 @@ def main():
     args = parser.parse_args()
     altered_file = args.input_file + args.extension
 
-    remove_blanks(args.input_file, altered_file)
+    remove_blanks_csv(args.input_file, altered_file)
+
 
 if __name__== "__main__":
   main()
